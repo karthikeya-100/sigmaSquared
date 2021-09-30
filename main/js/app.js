99cfb30c66dbb62861ded1ce37c2f1081d96b5ab
Karthikeya Sankaramanchi <karthikeya.sankaramanchi@gmail.com>
2021-09-30 17:43:47 +0530

    update in the logic

--- main/js/app.js
+++ main/js/app.js
@@ -13,75 +13,77 @@ current_image_name = document.querySelector("#myImage")
 
 // Code for Typewriting effect
 
-start_type = ()=>{
+start_type = () => {
     typewriter.typeString(title)
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("Sigma Squared.R")
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("Written By")
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("Big Data Analytics Dept.")
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("2016 Batch!!")
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("And Successfully extended")
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("By Other batches!")
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("Now it's Turn For!!")
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("2021 Batch!!!!")
-    .pauseFor(2500)
-    .deleteAll()
-    .start();
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString("Sigma Squared.R")
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString("Written By")
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString("Big Data Analytics Dept.")
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString("2016 Batch!!")
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString("And Successfully extended")
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString("By Other batches!")
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString("Now it's Turn For!!")
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString("2021 Batch!!!!")
+        .pauseFor(2500)
+        .deleteAll()
+        .start();
 }
 
 // Code for lighting lamps
-swap_image=(event) =>{
+swap_image = (event) => {
     name_value = parseInt(event.target.attributes[1].nodeValue)
-    switch(name_value){
+    switch (name_value) {
         case 1:
             current_image_name.src = "./img/l2.png"
             event.target.disabled = true
             break;
         case 2:
-            if(current_image_name.src.match("l2")){
+            if (current_image_name.src.match("l2")) {
                 current_image_name.src = "./img/l3.png"
                 event.target.disabled = true
             }
             break;
         case 3:
-            if(current_image_name.src.match("l3")){
+            if (current_image_name.src.match("l3")) {
                 current_image_name.src = "./img/l5.png"
                 event.target.disabled = true
-                main_title.style.display = "block"
-                ball.style.display = "inline-block"
-                start_type()
-                document.querySelector(".sample1").style.display = "inline-block"
-                document.querySelector(".sample2").style.display = "inline-block"
+                if (current_image_name.src.match("l5")) {
+                    main_title.style.display = "block"
+                    ball.style.display = "inline-block"
+                    start_type()
+                    document.querySelector(".sample1").style.display = "inline-block"
+                    document.querySelector(".sample2").style.display = "inline-block"
+                }
             }
-        break;
+            break;
         default:
             console.log("Error")
     }
 }
-guest1_button.addEventListener("click",(e)=>{
+guest1_button.addEventListener("click", (e) => {
     swap_image(e)
 })
 
-guest2_button.addEventListener("click",(e)=>{
+guest2_button.addEventListener("click", (e) => {
     swap_image(e)
 })
 
-guest3_button.addEventListener("click",(e)=>{
+guest3_button.addEventListener("click", (e) => {
     swap_image(e)
 
 })
